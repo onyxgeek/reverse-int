fb85d5095eb8baf20a0c199dafb4e906871c100d
onyxgeek <onyxpeek@gmail.com>
2023-02-14 12:07:41 +0200

    feat:finish the main promlem on the project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ module.exports = function reverse (n) {
    let a = Math.abs(n);
    let c = a.toString();
    let b = Array.from(c);
-   let m = b.reverse().join('');
-return m;
+   let ms = b.reverse().join('');
+return ms;
 }
